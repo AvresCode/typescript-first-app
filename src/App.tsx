76584e3cf81cf16c32cc3ae59825e73e92476723
AvresCode <servas230@gmail.com>
2022-10-14 21:36:49 +0200

    Added task input function

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import React, {FC} from 'react';
-import './App.css';
-
+import React, { FC, useState, ChangeEvent } from "react";
+import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
+    //console.log(task);
+  };
+
   return (
     <div className="App">
-    
-  <div> <h1>To Do List:</h1></div>
-  <div> <input className='inputContainer'  type="text" placeholder="Add a task"/></div>
-  <div> <button className='addButton'> Add Task</button></div>
-
+      <div>
+        {" "}
+        <h1>To Do List:</h1>
+      </div>
+      <div>
+        {" "}
+        <input
+          className="inputContainer"
+          type="text"
+          placeholder="Add a task"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        {" "}
+        <button className="addButton"> Add Task</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
